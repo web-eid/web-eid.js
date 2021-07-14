03aab5432543eb5fe741d4c6bd91d24247a37557
Raul Metsma <raul@metsma.ee>
2021-07-14 11:58:37 +0300

    Update license and copyright year
    WE2-386, WE2-506
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 The Web eID Project
+ * Copyright (c) Estonian Information System Authority
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
