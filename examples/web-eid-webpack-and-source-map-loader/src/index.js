603a36e63df769a82e3a643a3c1aae58f85a6545
Sven Mitt <svenzik@users.noreply.github.com>
2026-04-13 11:11:50 +0300

    print web-eid component versions to page
    WE2-968
    
    Signed-off-by: Sven Mitt <svenzik@users.noreply.github.com>

--- examples/web-eid-webpack-and-source-map-loader/src/index.js
+++ examples/web-eid-webpack-and-source-map-loader/src/index.js
@@ -1,18 +1,19 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-import * as WebEid from '@web-eid/web-eid-library';
+import * as WebEid from "@web-eid/web-eid-library";
 
 function component() {
-  WebEid.status().then(status => {
-    console.log(status);
-  });
-
   const element = document.createElement("div");
+  element.innerHTML = _.join(["Web eID demo"], " ");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  WebEid.status().then(status => {
+    element.innerHTML = _.join(
+      ["Web eID status:", JSON.stringify(status)],
+      " ",
+    );
+  });
 
   return element;
 }
 
 document.body.appendChild(component());
-
--- examples/web-eid-webpack-and-source-map-loader/webpack.config.js
+++ examples/web-eid-webpack-and-source-map-loader/webpack.config.js
@@ -1,15 +1,37 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "eval-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      watch: true,
+    },
+    devMiddleware: {
+      publicPath: "/",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Web eID demo - Webpack",
+      chunks: ["main"],
+      inject: "body",
+      scriptLoading: "defer",
+    }),
+  ],
   module: {
     rules: [
       {
